1ff6bf02966aaddab73e5011ef06c03d3a3536ce
Sgdghsja <3251266894@qq.com>
2024-10-28 00:27:48 +0800

    Update mysql.sql

--- demo/mysql.sql
+++ demo/mysql.sql
@@ -176,106 +176,11 @@ CREATE TABLE  `merchants` (
     merchant_id VARCHAR (8)
 );
 
-import java.util.Scanner;
-
-/**
- * 使用递归实现杨辉三角形并把每一行的数据存储到数组中
- */
-public class YangHui {
-    private int n; //总行数
-    private int[][] array; //存储前n行杨辉三角形的二维数组
-
-    /**
-     * 构造方法
-     */
-    public YangHui(){}
-
-    public YangHui(int n, int[][] array) {
-        this.n = n;
-        this.array = array;
-    }
-
-    /**
-     * 使用递归实现杨辉三角形
-     * @param a 行
-     * @param b 列
-     * @return
-     */
-    public int iter(int a, int b){
-        if (b == 1 || b == a){
-            return 1;
-        }else {
-            return iter(a-1,b-1) + iter(a-1,b);
-        }
-    }
-
-    /**
-     * 利用数组把杨辉三角形的数据存储到数组中，并打印
-     */
-    public void printNum() {
-        for (int i = 0; i < this.array.length; i++) { //行
-            for (int j = 0; j <= i; j++) { //列
-                //调用迭代方法存储到数组中
-                array[i][j] = iter(i + 1, j + 1);
-                //打印
-                System.out.print(array[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-
-    /**
-     * 主程序入口
-     * @param args
-     */
-    public static void main(String[] args) {                         
-        //1.使用扫描仪
-        Scanner sc = new Scanner(System.in);
-        System.out.print("请输入行数：");
-        int row = sc.nextInt();
-
-        //2.创建实例 并使用构造方法设置对象属性
-        YangHui yangHui = new YangHui(row,new int[row][row]);
-
-        //4.调用方法打印杨辉三角形
-        yangHui.printNum();
-
-    }
-}
-    //思路：使用栈来模拟递归函数
-    public static long solve(long m,long n){
-        Stack<Ack> stack = new Stack<>();
-        stack.push(new Ack(m,n));   //放入要求解的ack
-        //res用来记录ack(0,n)的值
-        //如果res大于0，则说明当前的函数层层调用(push)已经到底了，开始pop了。
-        long res = -1;
-        while(!stack.empty()){
-            Ack ack = stack.peek(); //对栈顶的ack进行分析
-            if(ack.m == 0){         //m为0,则求解出ack(0,n)的结果赋给res，并移出stack
-                res = ack.n+1;
-                stack.pop();
-            }else if(ack.n == 0 && ack.m > 0){
-                if(res < 0) {
-                    stack.push(new Ack(ack.m-1,1)); //res小于0，Ackermann(m,0) = Ackermann(m-1,1);
-                }
-                else {
-                    stack.pop();    //res大于0，则说明已经计算过了，可以pop
-                }
-            }else{
-                if(ack.data < 0){   //还没有赋值，只有维
-                    if(res < 0){
-                        stack.push(new Ack(ack.m,ack.n-1)); //计算Ackermann(m,n)所需要的Ackermann(m,n-1)
-                    }else {
-                        ack.data = res; //设置data是为了判断是否被计算
-                        res = -1;       //重置为1
-                        stack.push(new Ack(ack.m-1,ack.data));//Ackermann(m,n) = Ackermann(m-1,Ackermann(m,n-1))，这里的Ackermann(m,n-1)为data
-                    }
-                }else{
-                    stack.pop();    //已经计算出来的ack值被用到了，所以就pop出来
-                }
-            }
-        }
-        return res;
-    }
+create table 'comment'(
+    'id' bigint not null auto_increment comment '主键',
+    'user_id' bigint comment '用户id',
+    'order_id' bigint comment '订单id',
+    'comment_detail' varchar(200) comment '评价详情',
+    primary key ('id')
+)
 
